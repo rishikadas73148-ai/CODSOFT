c0d4bd18e6c5266dba87b0d309f409ad304525d2
Rishika Das <rishikadas73148@gmail.com>
2026-03-08 19:16:29 +0530

    Add files via upload

---
+++ python calculator.py
@@ -0,0 +1,16 @@
+num1=float(input("Enter first number: "))
+operator=input("Enter operator (+,-,*,/): ")
+num2=float(input("Enter second number: "))
+if operator=="+":
+    result=num1+num2
+elif operator=="-":
+    result=num1-num2
+elif operator=="*":
+    result=num1*num2
+elif operator=="/":
+    result=num1/num2
+else:
+    print("Invalid operator")
+    result=None
+if result!=None:
+    print("Result: ", result)
